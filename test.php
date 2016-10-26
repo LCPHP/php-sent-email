143a46095049a0c638f847d8799969b4428c1f5f
lucong <332553882@qq.com>
2016-10-26 20:01:51 +0800

    ad

--- test.php
+++ test.php
@@ -1,3 +1,31 @@
 <?php
 
 require_once 'vendor/autoload.php';
+
+use niklaslu\Mail;
+
+$mailConfig = [
+    'host' => '',
+    'username' => '',
+    'password' => '',
+    'post' => '',
+    //     'from' =>[
+        //         'address' => '',
+        //         'name' => ''
+        //     ],
+    'from' => '',
+
+];
+
+$mail = new Mail($mailConfig);
+
+$to = '332553882@qq.com';
+$subject = '测试';
+$body = '<h3>测试内容</h3>';
+$res = $mail->sent($to, $subject, $body);
+
+if (!$res){
+    print_r($mail->getError());
+}else{
+    echo '成功';
+}
