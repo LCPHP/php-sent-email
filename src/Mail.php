d3d1a56842a26dd649917d44ea901e56d73d6163
lucong <332553882@qq.com>
2016-10-26 20:00:22 +0800

    ad

---
+++ src/Mail.php
@@ -0,0 +1,116 @@
+<?php
+namespace niklaslu;
+class Mail {
+    
+    protected $config = [];
+    
+    protected $mail ; 
+    
+    protected $error = '';
+    
+    public function __construct($config){
+        
+        $mail = new \PHPMailer();
+        
+        if ($config['debug']){
+            $mail->SMTPDebug = 3;
+        }
+        
+        $mail->Host = $config['host'];
+        $mail->SMTPAuth = true;
+        $mail->Username = $config['username'];
+        $mail->Password = $config['password'];
+        $mail->SMTPSecure = isset($config['secure']) ? $config['secure'] : 'ssl';
+        $mail->Port = $config['port'];
+        
+        // 发信人
+        $from = $config['from'];
+        if (is_array($from)){
+            $address = $from['address'];
+            $fromName = $from['name'];
+        }else{
+            $address = $from;
+            $fromName = '';
+        }
+        $mail->setFrom($address , $fromName);
+        
+        $this->config = $config;
+        $this->mail = $mail;
+    }
+    
+    public function sent($to , $subject , $body , $altBody  = '', $replyTo = NULL , $attachments = null ){
+        
+        if (is_array($to)){
+            foreach ($to as $t){
+                $this->mail->addAddress($t);
+            }
+        }else{
+            $this->mail->addAddress($to);
+        }
+        
+        if ($replyTo){
+            $this->mail->addReplyTo($replyTo);
+        }
+        if ($attachments){
+            foreach ($attachments as $a){
+                if (is_array($a)){
+                    $this->mail->addAttachment($a[0] , $a[1]);
+                }else{
+                    $this->mail->addAttachment($a);
+                }
+                
+            }
+        }
+
+        $this->mail->isHTML(true);
+        $this->mail->Subject = $subject;
+        $this->mail->Body = $body;
+        $this->mail->AltBody = $altBody;
+        
+        if (!$this->mail->send()){
+            $this->error = $this->mail->ErrorInfo;
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
+    /**
+     * 添加抄送
+     */
+    public function addCC($arr){
+        
+        foreach ($arr as $a){
+            if (is_array($a)){
+                $this->mail->addCC($a[0] , $a[1]);
+            }else{
+                $this->mail->addCC($a);
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * 添加密送
+     */
+    public function addBCC($arr){
+        foreach ($arr as $a){
+            if (is_array($a)){
+                $this->mail->addBCC($a[0] , $a[1]);
+            }else{
+                $this->mail->addBCC($a);
+            }
+        }
+        return true;
+    }
+    
+    public function getConfig(){
+        
+        return $this->config;
+    }
+    
+    public function getError(){
+        
+        return $this->error;
+    }
+}
